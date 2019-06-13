55967296bf0f75ea7d0284f7c989e07872a2dfdf
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2019-06-13 18:38:15 +0200

    time limit

--- analysis/accuracy_test.py
+++ analysis/accuracy_test.py
@@ -1,27 +1,39 @@
 from tqdm import tqdm
 
-from functions import rastrigin_func
+from functions import matyas_func, rastrigin_func, levi_func
 from optimization.abc import ABC
 from optimization.eba import EBA
 from optimization.pso import PSO
 import matplotlib.pyplot as plt
+import time
 
+X_RESEARCH = 200
+
+
+def now():
+    return int(round(time.time() * 1000))
+
+
+fun = matyas_func
 iter_count = 10000
+time_limit = 1000
 plt.yscale('log')
 
-x = 5
-z = 100
+x = X_RESEARCH
 # --some-computation--
-pso = PSO(stop_func=lambda i: i < iter_count)
-pso_res = pso.optimize(x)
+start = now()
+pso = PSO(stop_func=lambda i: now() - start < time_limit)
+pso_res = pso.optimize(fun, x)
 # pso.optimize(x)
 
-abc = ABC(stop_func=lambda i: i < iter_count)
-abc_res = abc.optimize(x)
+start = now()
+abc = ABC(stop_func=lambda i: now() - start < time_limit)
+abc_res = abc.optimize(fun, x)
 # ba.optimize(x)
 
-eba = EBA(stop_func=lambda i: i < iter_count)
-eba_res = eba.optimize(x)
+start = now()
+eba = EBA(stop_func=lambda i: now() - start < time_limit)
+eba_res = eba.optimize(fun, x)
 
 print("PSO")
 plt.plot(pso_res, label="PSO")
